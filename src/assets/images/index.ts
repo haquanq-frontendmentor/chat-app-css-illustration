0332cf7df58ec23e026bc14f472a47600779e3de
Ha Quanq <quangha0120010@gmail.com>
2025-10-03 13:24:08 +0700

    Add static images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,4 @@
+export { default as UserAvatar } from "./avatar.jpg";
+export { default as DogImage1 } from "./dog-image-1.jpg";
+export { default as DogImage2 } from "./dog-image-2.jpg";
+export { default as DogImage3 } from "./dog-image-3.jpg";
